385c4651e8a27729abafe9ac6bdbc5743c7479bb
TaoMaru <jackson_maria1@columbusstate.edu>
2024-02-19 22:30:05 -0500

    Change config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/couch-overlay/",
-  path: `${__dirname}/docs/build`
+  base: "/couch-overlay",
+  path: `${__dirname}/docs/dist`
 })
